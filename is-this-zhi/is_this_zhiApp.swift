7cfbff5201e4c593d3646099b47bbc427eaa19e0
Zepto Zhou <xgzepto@gmail.com>
2026-04-23 23:01:32 +0800

    Initial Commit

---
+++ is-this-zhi/is_this_zhiApp.swift
@@ -0,0 +1,17 @@
+//
+//  is_this_zhiApp.swift
+//  is-this-zhi
+//
+//  Created by Zepto on 23/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct is_this_zhiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
